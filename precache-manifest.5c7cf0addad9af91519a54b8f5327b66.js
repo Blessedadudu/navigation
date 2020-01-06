88e9686c30464a026642c7ec495d58cf43cd783f
Blessedadudu <adududuke@gmail.com>
2020-01-06 15:14:38 +0100

    Updates

---
+++ precache-manifest.5c7cf0addad9af91519a54b8f5327b66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f04076f5df6a69811e9744d8eaff8b3c",
+    "url": "/navigation/index.html"
+  },
+  {
+    "revision": "a71626b5c4470c48f4c7",
+    "url": "/navigation/static/css/main.9f3f1155.chunk.css"
+  },
+  {
+    "revision": "426259e5eda0877acc2f",
+    "url": "/navigation/static/js/2.d5cd70a5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/navigation/static/js/2.d5cd70a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a71626b5c4470c48f4c7",
+    "url": "/navigation/static/js/main.589f2b20.chunk.js"
+  },
+  {
+    "revision": "27b0b7f10df0f8bc32be",
+    "url": "/navigation/static/js/runtime-main.4251e07a.js"
+  },
+  {
+    "revision": "231ffc4268fe1a82d8a502d1a5838023",
+    "url": "/navigation/static/media/joao-marcelo-martins-background-unsplash.231ffc42.jpg"
+  }
+]);
